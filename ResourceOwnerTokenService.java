3708e4985e80c13972e2c0d49b54f8d0bf55905e
jahnavenki <97650566+jahnavenki@users.noreply.github.com>
2024-05-24 18:23:47 +0530

    Update ResourceOwnerTokenService.java

--- ResourceOwnerTokenService.java
+++ ResourceOwnerTokenService.java
@@ -1,23 +1,26 @@
 package au.com.cfs.winged.core.models.common;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
-import java.io.IOException;
 
 public class ResourceOwnerTokenService {
 
-    private HttpClient httpClient;
+    private final HttpClient httpClient;
 
     public ResourceOwnerTokenService() {
-        this.httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build();
+        this.httpClient = HttpClient.newBuilder()
+                                    .version(HttpClient.Version.HTTP_2)
+                                    .build();
     }
 
-    public HttpRequest.Builder getHttpRequest() {
-        return httpClient.newRequestBuilder();
+    public HttpRequest.Builder getHttpRequestBuilder() {
+        return HttpRequest.newBuilder();
     }
 
+    public HttpResponse<String> sendRequest(HttpRequest request) throws IOException, InterruptedException {
+        return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+    }
 }
